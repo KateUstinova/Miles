929284ea5b36318f2e7e25924ffe16159038d4f7
Екатерина Устинова <dadsda@inbox.ru>
2022-06-19 18:50:49 +0700

    add

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int price = 195;
+        int mile = 20;
+
+        int bonus = price / mile;
+        System.out.println("Получено " + bonus + " бонусов");
+    }
+}
